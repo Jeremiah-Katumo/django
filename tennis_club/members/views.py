ec046fefa63ff579557f31b283fc527546348b62
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-07-20 13:17:37 +0300

    details

--- tennis_club/members/views.py
+++ tennis_club/members/views.py
@@ -2,8 +2,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from . import models
 
 # Create your views here.
 def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymembers = models.Member.objects.all().values
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    mymember = models.Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember': mymember
+    }
+    return HttpResponse(template.render(context, request))
