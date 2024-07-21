e45126ee546353e82132cbff55cb57f31ec570fb
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-07-22 00:27:52 +0300

    testing

--- tennis_club/members/views.py
+++ tennis_club/members/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.db.models import Q
 from . import models
 
 # Create your views here.
